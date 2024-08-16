a6127e248d6164585d5b1dacdffdcf321e7274e4
AdreanRodriguez <loco_bladare@hotmail.com>
2024-08-16 18:44:53 +0200

    the struggle continue

--- src/data/education.js
+++ src/data/education.js
@@ -1,83 +1,29 @@
 const education = [
     {
         id: 1,
-        education: "Utbildning",
         institution: "Folkuniversitetet",
         title: "Front-end utvecklare",
         url: "https://www.folkuniversitetet.se/vara-skolor/yh-utbildningar/alla-yh-utbildningar/frontendutvecklare/?_t_tags=language%3asv&_t_hit.id=Folkuniversitetet_Web_Models_Pages_YHArrangementPage%2f_1f224768-80d9-4545-91bf-0563c31b5714_sv&_t_hit.pos=1&bpid=63963",
         yearFrom: "Aug 2023",
         yearTo: "Jun 2025",
-        description: "Front-end utveckling vid Folkuniversitetet Karlstad, med fokus på UX/UI, HTML, CSS, JavaScript, agila metoder, Front-end ramverk, Full stack webbapplikation och Back-end med Node.js."
+        description: "Front-end utveckling Karlstad, med fokus på UX/UI, HTML, CSS, JavaScript, agila metoder, Front-end ramverk, Full stack webbapplikation och Back-end med Node.js."
     },
     {
-        id: 11,
-        year: "2018",
-        institution: "Grävmaskin, dumper & hjullastare",
-        description: "Certifikat för grävmaskin, dumper och hjullastare."
+        id: 2,
+        institution: "Yrkeshögskola i Enköping",
+        title: "Hiss- och rulltrappstekniker",
+        yearFrom: "Aug 2012",
+        yearTo: "Jul 2013",
+        description: "Ett års utbildning och erhållet certifikat som hiss- och rulltrappstekniker."
     },
     {
-        id: 10,
-        year: "2018",
-        institution: "Heta arbeten",
-        description: "Certifikat för heta arbeten."
+        id: 3,
+        institution: "Taserudsgymnasiet Arvika",
+        title: "Installations elektriker",
+        yearFrom: "Aug 2005",
+        yearTo: "Jul 2007",
+        description: "Examen och erhållet ECY-certifikat."
     },
-    // {
-    //     id: 9,
-    //     yearFrom: "2012",
-    //     yearTo: "2013",
-    //     institution: "Hissteknisk skola i Enköping",
-    //     description: "Ettårig utbildning inom hiss teknik och certifikat som hisstekniker."
-    // },
-    // {
-    //     id: 8,
-    //     year: "2012",
-    //     institution: "Lastkoppling",
-    //     description: "Utbildning i lastkoppling."
-    // },
-    // {
-    //     id: 7,
-    //     year: "2012",
-    //     institution: "Allmän ställningsbyggande utbildning",
-    //     description: "Utbildning i ställningsbyggande."
-    // },
-    // {
-    //     id: 6,
-    //     year: "2012",
-    //     institution: "Certifikat - Skötsel av elanläggningar",
-    //     description: "Certifikat för skötsel av elanläggningar."
-    // },
-    // {
-    //     id: 5,
-    //     year: "2011",
-    //     institution: "Mobilkranförarbevis",
-    //     description: "Certifikat för mobilkran."
-    // },
-    // {
-    //     id: 4,
-    //     year: "2009",
-    //     institution: "Truckkort",
-    //     description: "Lätt, mellan och tung (10 ton)."
-    // },
-    // {
-    //     id: 3,
-    //     year: "2008",
-    //     institution: "OLF grunkurs",
-    //     description: "Grundläggande säkerhets och beredskapsupplärning."
-    // },
-    // {
-    //     id: 2,
-    //     yearFrom: "2005",
-    //     yearTo: "2007",
-    //     institution: "Inriktning el-installation",
-    //     description: "ECY-certifikat."
-    // },
-    // {
-    //     id: 1,
-    //     yearFrom: "2004",
-    //     yearTo: "2005",
-    //     institution: "El Årjängs gymnasieskola",
-    //     description: "Elteknik."
-    // }
 ];
 
 export default education;
